f81c4b84d88d20b01db6e3bbba7005165fc61fd6
bagewadiarti-hub <bagewadi.arti@gmail.com>
2026-03-11 20:57:46 +0530

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,30 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 3.0"
+    }
+  }
+
+  backend "azurerm" {
+    resource_group_name  = "tfstate-rg"
+    storage_account_name = "tfstatestoragedrift"
+    container_name       = "tfstate"
+    key                  = "drift-detection.tfstate"
+  }
+}
+
+provider "azurerm" {
+  features {}
+}
+
+resource "azurerm_resource_group" "main" {
+  name     = var.resource_group_name
+  location = var.location
+
+  tags = {
+    Environment = var.environment
+    ManagedBy   = "Terraform"
+    Project     = "DriftDetection"
+  }
+}
